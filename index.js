a19d69917c8e22f47605f890fb8ce685c54080c6
prabinnnn <prabinbhandari120@gmail.com>
2024-02-08 23:45:29 +0545

    add index

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = expres();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const PORT = Number(process.env.PORT);
+const indexRouter = require("./routes");
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", indexRouter);
+mongoose.connect(process.env.DB).then(() => console.log("Connected!"));
